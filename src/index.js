03d3de88beb16f83b5b55f6f5e362e075235d5e5
Edgardo <Xotl@csxjq72.wvrgroup.internal>
2019-02-08 13:15:58 -0600

    Adding updated index.js

--- src/index.js
+++ src/index.js
@@ -2,22 +2,28 @@
 const Slack = require('./slack_api')
 const SmashLeague = require('./smash_league')
 const Ranking = require('../ranking.json')
+const Config = require('../config.json')
+const Utils = require('./utils')
 
-const SMASH_SLACK_CHANNEL_ID = 'GFY345QV8'
+const SMASH_SLACK_CHANNEL_ID = Config.slack_channel_id
+
 
 async function Main() {
-    let slackResponse
+    const now = new Date()
+    const lastInProgressUpdated = Utils.GetDateObjFromEpochTS(Ranking.in_progress.last_update_ts)
+    const opts = { latest: now, oldest: lastInProgressUpdated }
 
+    let slackResponse
     try {
-        slackResponse = await Slack.getMessagesFromPrivateChannel(SMASH_SLACK_CHANNEL_ID)
+        slackResponse = await Slack.getMessagesFromPrivateChannel(SMASH_SLACK_CHANNEL_ID, opts)
     }
     catch (err) { return console.error(err) }
 
     const activities = SmashLeague.categorizeSlackMessages(slackResponse.messages)
-    const newRankingObj = SmashLeague.digestActivitiesAndGetUpdatedRankingObj(activities, Ranking)
-    
-    console.log('Wow, such activities', activities.challenges, activities.reportedResults)
-    console.log('Wow, such new ranking', newRankingObj)
+    const newInProgressObj = SmashLeague.digestActivitiesAndGetUpdatedRankingObj(activities, Ranking)
+
+    console.log('Wow, such newInProgressObj', newInProgressObj)
+    console.log('Wow, such ranking', SmashLeague.getRankingFromScoreboard(newInProgressObj.scoreboard))
 }
 
 Main().catch(console.error)
