625a1c1c83cd6275e66c88c73def2cb650f6010c
Edgardo <Xotl@IT1s-MacBook-Pro.local>
2019-02-09 15:05:07 -0600

    Updating ranking info & paths

--- src/output-generator.js
+++ src/output-generator.js
@@ -65,7 +65,7 @@ const updateRankingMarkdownFile = async rankingObj => {
     return new Promise(
         (resolve, reject) => {
             fs.writeFile(
-                path.join(__dirname, '../', 'release', 'RANKING.md'),
+                path.join(__dirname, '../', 'ranking-info', 'RANKING.md'),
                 output,
                 (err) => {
                     if (err) {
