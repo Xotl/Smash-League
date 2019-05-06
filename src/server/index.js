7209d93fbd618c6a5c49111f8062e19c02dd08a8
Edgardo <Xotl@IT1s-MacBook-Pro.local>
2019-05-05 22:48:07 -0500

    Adding logs

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('¡Si charcha!'))
+app.get('/', (req, res) => {
+    console.log(`[${(new Date()).toISOString()}]`, req.originalUrl, req.query, req.body)
+    return res.send('¡Si charcha!')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
